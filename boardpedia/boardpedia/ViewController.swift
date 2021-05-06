9019023c167ac7591ba67856a687338e7ce5597f
xwoud <xwoud@naver.com>
2021-05-06 17:19:27 +0900

    [upload] 보드피디아 프로젝트 생성
    body: Xcode를 이용한 프로젝트 생성
    
    Related to: #1

---
+++ boardpedia/boardpedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
