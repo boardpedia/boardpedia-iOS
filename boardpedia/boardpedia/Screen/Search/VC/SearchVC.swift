ea9cabe0b3907d6d5c660e7a3594264a049162d6
xwoud <xwoud@naver.com>
2021-05-11 01:04:00 +0900

    [fix] searchbar 오류 수정
    body: 클린 폴더 하니깐 해결

--- boardpedia/boardpedia/Screen/Search/VC/SearchVC.swift
+++ boardpedia/boardpedia/Screen/Search/VC/SearchVC.swift
@@ -30,12 +30,19 @@ class SearchVC: UIViewController {
 //        self.presentView.addSubview(vc.view)
 //        vc.didMove(toParent: self)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 뷰 클릭 시 키보드 내리기
+        view.endEditing(true)
+    }
 
 }
 
 // MARK: Extension
 
 extension SearchVC {
+    
+    // MARK: View Style Function
     
     func setView() {
         
@@ -44,6 +51,22 @@ extension SearchVC {
         searchTextField.addLeftPadding()
         searchTextField.placeholder = "원하는 보드게임을 찾아보세요!"
         searchTextField.font = .neoMedium(ofSize: 17)
+        searchTextField.delegate = self
     
     }
+}
+
+// MARK: UITextFieldDelegate
+
+extension SearchVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        searchTextField.becomeFirstResponder()
+    }
+    
 }
