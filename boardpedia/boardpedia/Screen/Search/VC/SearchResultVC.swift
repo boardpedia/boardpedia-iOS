3f30b5f0ee6c10662253221be4d0250b6cd9053d
xwoud <xwoud@naver.com>
2021-05-11 17:53:29 +0900

    [update] data 추가
    body: 두줄 이상의 데이터 추가
    
    Related to: #10

--- boardpedia/boardpedia/Screen/Search/VC/SearchResultVC.swift
+++ boardpedia/boardpedia/Screen/Search/VC/SearchResultVC.swift
@@ -107,8 +107,9 @@ extension SearchResultVC {
         
         // Test Data (서버 연결 전)
         let themeItem1 = SearchResultData(gameImage: "testImage", gameName: "할리갈리 디럭스", gameInfo: "벨과 함께 즐기는 스릴감", saveNumber: 100, startNumber: 4.5, bookMark: false)
+        let themeItem2 = SearchResultData(gameImage: "testImage", gameName: "오늘의 일기 김민희", gameInfo: "오늘은 굉장히 더운날이다. 미쳤다. 여름에는 얼마나 더울까?", saveNumber: 98, startNumber: 3, bookMark: true)
         
-        searchResultData.append(contentsOf: [themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1])
+        searchResultData.append(contentsOf: [themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem2])
         
         
         searchResultCollectionView.delegate = self
