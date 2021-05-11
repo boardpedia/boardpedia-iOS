b962ed23793be7e3f76f6cab735b5bfd298ad9a4
xwoud <xwoud@naver.com>
2021-05-11 14:39:55 +0900

    [upload] 검색 결과 창 VC 생성
    body: SearchResultVC 생성
    
    Related to: #10

---
+++ boardpedia/boardpedia/Screen/Search/VC/SearchResultVC.swift
@@ -0,0 +1,32 @@
+//
+//  SearchResultVC.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/05/11.
+//
+
+import UIKit
+
+class SearchResultVC: UIViewController {
+
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+    
+    // MARK: IBAction
+    
+    // MARK: Life Cycle Part
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+// MARK: Extension
+
+extension SearchResultVC {
+    
+}
