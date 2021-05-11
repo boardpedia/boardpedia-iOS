8c14601a3a61c3be21eb405b09349345605408f3
xwoud <xwoud@naver.com>
2021-05-12 00:08:46 +0900

    [style] XLPagerTabStrip 라이브러리 적용중

--- boardpedia/boardpedia/Screen/Mypage/VC/MypageVC.swift
+++ boardpedia/boardpedia/Screen/Mypage/VC/MypageVC.swift
@@ -6,43 +6,51 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class MypageVC: UIViewController {
+class MypageVC: ButtonBarPagerTabStripViewController {
     
     // MARK: Variable Part
     
     // MARK: IBOutlet
-
+    
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nickLabel: UILabel!
     @IBOutlet weak var levelButton: UIButton!
     
-    @IBOutlet weak var saveListButton: UIButton!
-    @IBOutlet weak var reviewListButton: UIButton!
-    
-    @IBOutlet weak var listView: UIView!
-    
     // MARK: IBAction
     
-    @IBAction func saveListButtonDidTap(_ sender: Any) {
-        
-        saveListButton.setButton(text: "저장 목록", font: .neoSemiBold(ofSize: 16))
-        reviewListButton.setButton(text: "후기 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
-        
-    }
-    
-    @IBAction func reviewListButtonDidTap(_ sender: Any) {
-        
-        reviewListButton.setButton(text: "후기 목록", font: .neoSemiBold(ofSize: 16))
-        saveListButton.setButton(text: "저장 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
-    }
+    //    @IBAction func saveListButtonDidTap(_ sender: Any) {
+    //
+    //        saveListButton.setButton(text: "저장 목록", font: .neoSemiBold(ofSize: 16))
+    //        reviewListButton.setButton(text: "후기 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
+    //
+    //    }
+    //
+    //    @IBAction func reviewListButtonDidTap(_ sender: Any) {
+    //
+    //        reviewListButton.setButton(text: "후기 목록", font: .neoSemiBold(ofSize: 16))
+    //        saveListButton.setButton(text: "저장 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
+    //    }
     // MARK: Life Cycle Part
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setView()
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+//        let saveList = UIStoryboard.init(name: "Search", bundle: nil).instantiateViewController(withIdentifier: "SearchResultVC") as! SearchResultVC
+//        saveList.tabName = "저장 목록"
+////        saveList.nibName
+//
+//
+////        let reviewList = UIStoryboard(name: "Calendar", bundle: nil).instantiateViewController(withIdentifier: "CalendarVC") as! MypageVC
+//        reviewList.tabName = "후기 목록"
+        
+        return []
     }
     
 }
@@ -52,6 +60,8 @@ class MypageVC: UIViewController {
 extension MypageVC {
     
     func setView() {
-        saveListButtonDidTap(self)
+        //        saveListButtonDidTap(self)
     }
+    
+    
 }
