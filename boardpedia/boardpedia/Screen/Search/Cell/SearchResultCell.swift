1ce425bb84a82906451cfc82d41a1c957a7e153b
xwoud <xwoud@naver.com>
2021-05-11 17:10:53 +0900

    [style] 결과 보드게임 보여주기 레이아웃 잡기
    body: 검색 시 나오는 보드게임 결과 CollectionView로 구성
    
    Related to: #10

---
+++ boardpedia/boardpedia/Screen/Search/Cell/SearchResultCell.swift
@@ -0,0 +1,47 @@
+//
+//  SearchResultCell.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/05/11.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+    
+    // MARK: Variable Part
+    
+    static let identifier = "SearchResultCell"
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var boardImageView: UIImageView!
+    @IBOutlet weak var boardGameNameLabel: UILabel!
+    @IBOutlet weak var boardGameInfoLabel: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var gameValueLabel: UILabel!
+    
+    
+    // MARK: ContentView Default Set Function
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.contentView.backgroundColor = .boardWhite
+        self.contentView.setRounded(radius: 6)
+        
+        boardGameNameLabel.numberOfLines = 0
+        boardGameInfoLabel.numberOfLines = 0
+        boardImageView.setRounded(radius: 6)
+    }
+    
+    // MARK: Data Set Function
+    func configure(image: String, name: String, info: String, star: Float, save: Int) {
+
+        boardImageView.image = UIImage(named: image)
+        boardGameNameLabel.setLabel(text: name, font: .neoMedium(ofSize: 16))
+        boardGameInfoLabel.setLabel(text: info, color: .boardGray50, font: .neoRegular(ofSize: 13))
+        gameValueLabel.setLabel(text: "별점 \(star) / 저장 \(save)회", color: .boardGray40, font: .neoMedium(ofSize: 12))
+        
+    }
+}
--- boardpedia/boardpedia/Screen/Search/VC/SearchResultVC.swift
+++ boardpedia/boardpedia/Screen/Search/VC/SearchResultVC.swift
@@ -11,9 +11,10 @@ class SearchResultVC: UIViewController {
     
     // MARK: Variable Part
     
-    var number: Int = 0 {
-        // number가 바뀔 때 마다 실행
+    var searchResultData: [SearchResultData] = [] {
         didSet {
+            // 데이터 값이 바뀔 때 마다 라벨 변경
+            
             setResultLabel()
         }
     }
@@ -22,6 +23,7 @@ class SearchResultVC: UIViewController {
     
     @IBOutlet var filterButton: [UIButton]!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var searchResultCollectionView: UICollectionView!
     
     // MARK: IBAction
     
@@ -30,7 +32,7 @@ class SearchResultVC: UIViewController {
         for i in Range(0...3) {
             if filterButton[i].isTouchInside {
                 // 터치된 특정 인덱스 찾기
-//                number += 1
+                
                 filterButton[i].isSelected = !filterButton[i].isSelected
                 // 활성화 <--> 비활성화
                 
@@ -57,6 +59,7 @@ class SearchResultVC: UIViewController {
         super.viewDidLoad()
         setButton()
         setResultLabel()
+        setResultCollectionView()
     }
     
 }
@@ -83,12 +86,12 @@ extension SearchResultVC {
     // MARK: Result Count Label Style Function
     
     func setResultLabel() {
-        resultLabel.setLabel(text: "전체 \(number)개의 검색 결과가 있어요!", color: .boardGray40, font: .neoMedium(ofSize: 15))
+        resultLabel.setLabel(text: "전체 \(searchResultData.count)개의 검색 결과가 있어요!", color: .boardGray40, font: .neoMedium(ofSize: 15))
         
         if let text = resultLabel.text {
             // 앞부분만 폰트와 컬러를 다르게 설정
             
-            let changeString: String = "\(number)개"
+            let changeString: String = "\(searchResultData.count)개"
             let attributedStr = NSMutableAttributedString(string: text)
             
             attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String), value: UIFont.neoSemiBold(ofSize: 15), range: (text as NSString).range(of: changeString))
@@ -97,4 +100,80 @@ extension SearchResultVC {
             resultLabel.attributedText = attributedStr
         }
     }
+    
+    // MARK: Result Data Style Function
+    
+    func setResultCollectionView() {
+        
+        // Test Data (서버 연결 전)
+        let themeItem1 = SearchResultData(gameImage: "testImage", gameName: "할리갈리 디럭스", gameInfo: "벨과 함께 즐기는 스릴감", saveNumber: 100, startNumber: 4.5)
+        
+        searchResultData.append(contentsOf: [themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1,themeItem1])
+        
+        
+        searchResultCollectionView.delegate = self
+        searchResultCollectionView.dataSource = self
+        searchResultCollectionView.backgroundColor = .boardGray
+    }
 }
+
+
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension SearchResultVC: UICollectionViewDelegateFlowLayout {
+    // CollectionView 크기 잡기
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 한 아이템의 크기
+        
+        let itemWidth = self.view.frame.width - 32
+        return CGSize(width: itemWidth, height: 120/343 * itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // 아이템간의 간격
+        
+        return 10
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        // collectionView와 View 간의 간격
+        
+        return UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        
+    }
+    
+}
+
+// MARK: UICollectionViewDataSource
+
+extension SearchResultVC: UICollectionViewDataSource {
+    // CollectionView 데이터 넣기
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return searchResultData.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCell.identifier, for: indexPath) as? SearchResultCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configure(image: searchResultData[indexPath.row].gameImage, name: searchResultData[indexPath.row].gameName, info: searchResultData[indexPath.row].gameInfo, star: searchResultData[indexPath.row].startNumber, save: searchResultData[indexPath.row].saveNumber)
+        return cell
+        
+    }
+    
+}
+
