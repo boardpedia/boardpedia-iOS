e877a798002e2ede44d9ac85e4615b318e946960
xwoud <xwoud@naver.com>
2021-05-11 23:43:01 +0900

    [style] 버튼 생성중

--- boardpedia/boardpedia/Screen/Mypage/VC/MypageVC.swift
+++ boardpedia/boardpedia/Screen/Mypage/VC/MypageVC.swift
@@ -8,22 +8,50 @@
 import UIKit
 
 class MypageVC: UIViewController {
+    
+    // MARK: Variable Part
+    
+    // MARK: IBOutlet
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nickLabel: UILabel!
+    @IBOutlet weak var levelButton: UIButton!
+    
+    @IBOutlet weak var saveListButton: UIButton!
+    @IBOutlet weak var reviewListButton: UIButton!
+    
+    @IBOutlet weak var listView: UIView!
+    
+    // MARK: IBAction
+    
+    @IBAction func saveListButtonDidTap(_ sender: Any) {
+        
+        saveListButton.setButton(text: "저장 목록", font: .neoSemiBold(ofSize: 16))
+        reviewListButton.setButton(text: "후기 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
+        
+    }
+    
+    @IBAction func reviewListButtonDidTap(_ sender: Any) {
+        
+        reviewListButton.setButton(text: "후기 목록", font: .neoSemiBold(ofSize: 16))
+        saveListButton.setButton(text: "저장 목록", color: .boardGray50, font: .neoMedium(ofSize: 16))
+    }
+    // MARK: Life Cycle Part
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: Extension
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MypageVC {
+    
+    func setView() {
+        saveListButtonDidTap(self)
     }
-    */
-
 }
