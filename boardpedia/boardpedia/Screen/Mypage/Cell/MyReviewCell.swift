c3d616f4dafaaa1a81016866e7cc529cb45d38c9
xwoud <xwoud@naver.com>
2021-05-24 17:34:23 +0900

    [style] 후기 뷰 tableView로 구성 변경
    body: 후기 삭제 스와이프 모션을 고려해 tableview로 최종 변경
    
    Related to: #12

--- boardpedia/boardpedia/Screen/Mypage/Cell/MyReviewCell.swift
+++ boardpedia/boardpedia/Screen/Mypage/Cell/MyReviewCell.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class MyReviewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
+class MyReviewCell: UITableViewCell {
 
     static let identifier = "MyReviewCell"
-    var pan: UIPanGestureRecognizer!
-    var deleteLabel2: UIButton!
     
+    @IBOutlet weak var seperateView: UIView!
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var gameNameLabel: UILabel!
     @IBOutlet weak var firstKeywordLabel: UILabel!
@@ -22,60 +21,25 @@ class MyReviewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        commonInit()
+        defaultSettingView()
     }
     
-    private func commonInit() {
-
-        self.contentView.setRounded(radius: 6)
-        self.contentView.backgroundColor = .boardWhite
+    // MARK: Default View Setting Function
+    
+    func defaultSettingView() {
         
+        self.contentView.backgroundColor = .boardGray
+        seperateView.backgroundColor = .white
+        seperateView.setRounded(radius: 6)
         gameImageView.setRounded(radius: 6)
         gameNameLabel.setLabel(text: "", font: .neoMedium(ofSize: 16))
         firstKeywordLabel.setBorder(borderColor: .boardOrange, borderWidth: 1)
         secondKeywordLabel.setBorder(borderColor: .boardOrange, borderWidth: 1)
         firstKeywordLabel.setLabel(text: "", color: .boardOrange, font: .neoMedium(ofSize: 15))
         secondKeywordLabel.setLabel(text: "", color: .boardOrange, font: .neoMedium(ofSize: 15))
-
-        deleteLabel2 = UIButton()
-        deleteLabel2.setButton(text: "Delete", color: .white, font: .neoBold(ofSize: 16), backgroundColor: .boardOrange)
-        self.insertSubview(deleteLabel2, belowSubview: self.contentView)
-
-        pan = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
-        pan.delegate = self
-        self.addGestureRecognizer(pan)
-      }
+    }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        if (pan.state == UIGestureRecognizer.State.changed) {
-          let p: CGPoint = pan.translation(in: self)
-          let width = self.contentView.frame.width
-          let height = self.contentView.frame.height
-          self.contentView.frame = CGRect(x: p.x,y: 0, width: width, height: height);
-          self.deleteLabel2.frame = CGRect(x: p.x + width + deleteLabel2.frame.size.width, y: 0, width: 100, height: height)
-        }
-
-      }
-    @objc func onPan(_ pan: UIPanGestureRecognizer) {
-        if pan.state == UIGestureRecognizer.State.began {
-
-        } else if pan.state == UIGestureRecognizer.State.changed {
-          self.setNeedsLayout()
-        } else {
-          if abs(pan.velocity(in: self).x) > 500 {
-            let collectionView: UICollectionView = self.superview as! UICollectionView
-            let indexPath: IndexPath = collectionView.indexPathForItem(at: self.center)!
-            collectionView.delegate?.collectionView!(collectionView, performAction: #selector(onPan(_:)), forItemAt: indexPath, withSender: nil)
-          } else {
-            UIView.animate(withDuration: 0.4, animations: {
-              self.setNeedsLayout()
-              self.layoutIfNeeded()
-            })
-          }
-        }
-      }
+    
     
     // MARK: Data Set Function
     
@@ -83,8 +47,8 @@ class MyReviewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
 
         gameImageView.image = UIImage(named: image)
         gameNameLabel.text = name
-        firstKeywordLabel.text = "  \(first)  "
-        secondKeywordLabel.text = "  \(second)  "
+        firstKeywordLabel.text = "\(first)"
+        secondKeywordLabel.text = "\(second)"
         firstKeywordLabel.setRounded(radius: 11)
         secondKeywordLabel.setRounded(radius: 11)
         
--- boardpedia/boardpedia/Screen/Mypage/VC/MyReviewListVC.swift
+++ boardpedia/boardpedia/Screen/Mypage/VC/MyReviewListVC.swift
@@ -13,20 +13,23 @@ class MyReviewListVC: UIViewController {
     
     var myReviewData: [MyReviewData] = []
     
-    @IBOutlet weak var MyReviewListCollectionView: UICollectionView!
+    @IBOutlet weak var MyReviewListTableView: UITableView!
     
     // MARK: Life Cycle Part
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        MyReviewListCollectionView.delegate = self
-        MyReviewListCollectionView.dataSource = self
-        MyReviewListCollectionView.backgroundColor = .boardGray
-        setResultCollectionView()
+        
+        setResultTableView()
         // Do any additional setup after loading the view.
     }
     
-    func setResultCollectionView() {
+    func setResultTableView() {
+        
+        MyReviewListTableView.delegate = self
+        MyReviewListTableView.dataSource = self
+        MyReviewListTableView.backgroundColor = .boardGray
+        self.view.backgroundColor = .boardGray
         
         // Test Data (서버 연결 전)
         let item1 = MyReviewData(gameImage: "testImage", gameName: "할리갈리 디럭스", firstKeyword: "스피드", secondKeyword: "꿀잼보장", star: 5)
@@ -34,65 +37,40 @@ class MyReviewListVC: UIViewController {
         let item3 = MyReviewData(gameImage: "testBackImage_1", gameName: "보드피디아", firstKeyword: "귀요미", secondKeyword: "3명", star: 3.5)
         myReviewData.append(contentsOf: [item1,item2,item3,item1,item2,item3])
         
+        MyReviewListTableView.backgroundColor = .boardGray
+        
     }
 }
 
 
-// MARK: UICollectionViewDelegateFlowLayout
+// MARK: UITableViewDataSource
 
-extension MyReviewListVC: UICollectionViewDelegateFlowLayout {
-    // CollectionView 크기 잡기
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // 한 아이템의 크기
-        
-        let itemWidth = self.view.frame.width - 32
-        return CGSize(width: itemWidth, height: 120/343 * itemWidth)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        // 아이템간의 간격
-        
-        return 10
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        
-        return 0
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        // collectionView와 View 간의 간격
-        
-        return UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
-        
-    }
-    
-}
-
-// MARK: UICollectionViewDataSource
-
-extension MyReviewListVC: UICollectionViewDataSource {
-    // CollectionView 데이터 넣기
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension MyReviewListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return myReviewData.count
         
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyReviewCell.identifier, for: indexPath) as? MyReviewCell else {
-            return UICollectionViewCell()
-        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyReviewCell.identifier, for: indexPath) as? MyReviewCell else { return UITableViewCell() }
         
         cell.configure(image: myReviewData[indexPath.row].gameImage, name: myReviewData[indexPath.row].gameName, first: myReviewData[indexPath.row].firstKeyword, second: myReviewData[indexPath.row].secondKeyword, star: myReviewData[indexPath.row].star)
+        
         return cell
         
+    }
+    
+    
+}
+
+// MARK: UITableViewDelegate
+
+extension MyReviewListVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
     }
     
     
