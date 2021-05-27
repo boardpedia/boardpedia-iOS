72e37d11be2eb26f8bb6b91a752958352f707b3b
xwoud <xwoud@naver.com>
2021-05-27 21:01:06 +0900

    [upload] GameCollct VC, Storyboard 생성

---
+++ boardpedia/boardpedia/Screen/Collect/VC/GameCollectVC.swift
@@ -0,0 +1,29 @@
+//
+//  GameCollectVC.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/05/27.
+//
+
+import UIKit
+
+class GameCollectVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
