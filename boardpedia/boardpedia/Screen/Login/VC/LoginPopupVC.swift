c666f3ab19b69d3f471a1d8b44e8cdde0f9aa9ed
xwoud <xwoud@naver.com>
2021-06-05 15:41:52 +0900

    [upload] 로그인 팝업 뷰 생성

---
+++ boardpedia/boardpedia/Screen/Login/VC/LoginPopupVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginPopupVC.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/06/05.
+//
+
+import UIKit
+
+class LoginPopupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
