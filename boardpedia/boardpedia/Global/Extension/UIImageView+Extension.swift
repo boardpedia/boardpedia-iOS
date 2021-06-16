7b574b90dcaf316e57f355ae0b6551904cfd7eef
xwoud <xwoud@naver.com>
2021-06-16 15:00:52 +0900

    [upload]  UIImageView+Extension 생성

---
+++ boardpedia/boardpedia/Global/Extension/UIImageView+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIImageView+Extension.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/06/16.
+//
+
+import Foundation
+import Kingfisher
+
+
+extension UIImageView {
+    func setImage(from url: String) {
+        self.kf.indicatorType = .activity
+        let cache = ImageCache.default
+        
+        cache.retrieveImage(forKey: url) { result in
+            switch result {
+            case .success(let value):
+                if value.image != nil {
+                    self.image = value.image
+                    
+                } else {
+                    self.kf.setImage(with: URL(string: url))
+                    
+                }
+            case .failure(let err):
+                print(err.errorCode)
+            }
+        }
+        
+    }
+}
+
