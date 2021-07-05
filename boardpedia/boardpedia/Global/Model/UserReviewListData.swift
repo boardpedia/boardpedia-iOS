e261163f81868214313af0a03f3ba922bf248604
xwoud <xwoud@naver.com>
2021-07-05 23:44:36 +0900

    [network] 내 후기 가져오기 서버 연ㄱ
    body : 로그인 시 내가 작성한 후기 가져오기 완료
    
    Related to: #36

--- boardpedia/boardpedia/Global/Model/UserReviewListData.swift
+++ boardpedia/boardpedia/Global/Model/UserReviewListData.swift
@@ -12,11 +12,23 @@ struct UserReviewListData: Codable {
     let reviewIdx: Int
     let star: Double
     let keyword: [String]
-    let boardGame: BoardGame
+    let boardGame: ReviewBoardGame
 
     enum CodingKeys: String, CodingKey {
         case reviewIdx = "ReviewIdx"
         case star, keyword
         case boardGame = "BoardGame"
+    }
+}
+
+struct ReviewBoardGame: Codable {
+    let gameIdx: Int
+    let name: String
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case gameIdx = "GameIdx"
+        case name
+        case imageURL = "imageUrl"
     }
 }
--- boardpedia/boardpedia/Screen/Mypage/Cell/MyReviewCell.swift
+++ boardpedia/boardpedia/Screen/Mypage/Cell/MyReviewCell.swift
@@ -10,13 +10,19 @@ import UIKit
 class MyReviewCell: UITableViewCell {
 
     static let identifier = "MyReviewCell"
+    var keywordData: [String] = []
     
     @IBOutlet weak var seperateView: UIView!
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var gameNameLabel: UILabel!
-    @IBOutlet weak var firstKeywordLabel: UILabel!
-    @IBOutlet weak var secondKeywordLabel: UILabel!
+    @IBOutlet weak var keywordCollectionView: UICollectionView!
     @IBOutlet var starImageView: [UIImageView]!
+    
+    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout! {
+        didSet {
+            collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+    }
     
     
     override func awakeFromNib() {
@@ -33,27 +39,30 @@ class MyReviewCell: UITableViewCell {
         seperateView.setRounded(radius: 6)
         gameImageView.setRounded(radius: 6)
         gameNameLabel.setLabel(text: "", font: .neoMedium(ofSize: 16))
-        firstKeywordLabel.setBorder(borderColor: .boardOrange, borderWidth: 1)
-        secondKeywordLabel.setBorder(borderColor: .boardOrange, borderWidth: 1)
-        firstKeywordLabel.setLabel(text: "", color: .boardOrange, font: .neoMedium(ofSize: 15))
-        secondKeywordLabel.setLabel(text: "", color: .boardOrange, font: .neoMedium(ofSize: 15))
+
+        let layout = keywordCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.scrollDirection = .vertical
+        keywordCollectionView.delegate = self
+        keywordCollectionView.dataSource = self
+        keywordCollectionView.backgroundColor = .none
+        
+        let customLayout = LeftAlignFlowLayout()
+        keywordCollectionView.collectionViewLayout = customLayout
+        customLayout.estimatedItemSize = CGSize(width: 10, height: 20)
     }
-    
     
     
     // MARK: Data Set Function
     
-    func configure(image: String, name: String, first: String, second: String, star: Float) {
+    func configure(image: String, name: String, keyword: [String], star: Double) {
 
-        gameImageView.image = UIImage(named: image)
+        gameImageView.setImage(from: image)
         gameNameLabel.text = name
-        firstKeywordLabel.text = "\(first)"
-        secondKeywordLabel.text = "\(second)"
-        firstKeywordLabel.setRounded(radius: 11)
-        secondKeywordLabel.setRounded(radius: 11)
+        keywordData = keyword
+        keywordCollectionView.reloadData()
         
         for i in Range(0...4) {
-            if Float(i) < star {
+            if Double(i) < star {
                 starImageView[i].image = UIImage(named: "icStar")
             } else {
                 starImageView[i].image = UIImage(named: "icStarBlank")
@@ -63,3 +72,58 @@ class MyReviewCell: UITableViewCell {
     }
 
 }
+
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension MyReviewCell: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 한 아이템의 크기
+        
+        return CGSize(width: 36, height: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // 아이템간의 간격
+        
+        return 0
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        // collectionView와 View 간의 간격
+        
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+    }
+    
+}
+
+// MARK: UICollectionViewDataSource
+
+extension MyReviewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return keywordData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThemeKeywordCell.identifier, for: indexPath) as? ThemeKeywordCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(title: keywordData[indexPath.row])
+    
+        return cell
+    }
+    
+    
+}
+
+
