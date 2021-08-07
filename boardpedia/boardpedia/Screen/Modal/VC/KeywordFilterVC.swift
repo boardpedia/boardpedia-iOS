1053e845b69894ccc57ec7a46c0d9f80202d7726
xwoud <xwoud@naver.com>
2021-08-07 14:44:53 +0900

    [style] object 위치 생성

---
+++ boardpedia/boardpedia/Screen/Modal/VC/KeywordFilterVC.swift
@@ -0,0 +1,33 @@
+//
+//  KeywordFilterVC.swift
+//  boardpedia
+//
+//  Created by 김민희 on 2021/08/07.
+//
+
+import UIKit
+
+class KeywordFilterVC: UIViewController {
+
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var keywordCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
