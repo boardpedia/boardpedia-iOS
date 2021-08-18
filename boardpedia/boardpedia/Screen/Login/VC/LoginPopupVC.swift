18a2fd7f940d2a685ac1638b026a1cffd4137920
xwoud <xwoud@naver.com>
2021-08-18 18:47:16 +0900

    [feat] apple Login 연동

--- boardpedia/boardpedia/Screen/Login/VC/LoginPopupVC.swift
+++ boardpedia/boardpedia/Screen/Login/VC/LoginPopupVC.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class LoginPopupVC: UIViewController {
+class LoginPopupVC: UIViewController, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
 
+    @IBOutlet weak var appleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let appleLoginButton = ASAuthorizationAppleIDButton(type: .continue, style: .white)
+        
+        appleLoginButton.layer.borderWidth = 1
+        appleLoginButton.layer.borderColor = UIColor.black.cgColor
+        appleLoginButton.setRounded(radius: 8)
 
+        appleLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        appleLoginButton.widthAnchor.constraint(equalToConstant: self.view.frame.width-32).isActive = true
+        appleLoginButton.heightAnchor.constraint(equalToConstant: appleView.frame.height).isActive = true
+        
+        appleLoginButton.addTarget(self, action: #selector(appleLoginButtonDidTap), for: .touchUpInside)
+        appleView.addSubview(appleLoginButton)
         // Do any additional setup after loading the view.
     }
     
@@ -25,6 +44,37 @@ class LoginPopupVC: UIViewController {
             
             
         }
+    }
+    
+    @objc func appleLoginButtonDidTap() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+           let request = appleIDProvider.createRequest()
+           request.requestedScopes = [.fullName, .email]
+               
+           let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+           authorizationController.delegate = self
+           authorizationController.presentationContextProvider = self
+           authorizationController.performRequests()
+    }
+    // Apple ID 연동 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                
+            // 계정 정보 가져오기
+            let userIdentifier = appleIDCredential.user
+                //000684.f8590ae737d74fa5ad4a5120b0432991.1718
+            print("User ID : \(userIdentifier)")
+     
+        default:
+            break
+        }
+    }
+        
+    // Apple ID 연동 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
     }
 
 }
--- boardpedia/boardpedia/Support/Delegate/AppDelegate.swift
+++ boardpedia/boardpedia/Support/Delegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: "000684.f8590ae737d74fa5ad4a5120b0432991.1718") { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                // The Apple ID credential is valid.
+                print("해당 ID는 연동되어있습니다.")
+            case .revoked:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                print("해당 ID는 연동되어있지않습니다.")
+            case .notFound:
+                // The Apple ID credential is either was not found, so show the sign-in UI.
+                print("해당 ID를 찾을 수 없습니다.")
+            default:
+                break
+            }
+        }
+        
         return true
     }
 
