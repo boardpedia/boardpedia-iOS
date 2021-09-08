db7b15a4b62bd64ae20363ee49736c5ac6e2404a
xwoud <xwoud@naver.com>
2021-09-08 21:43:29 +0900

    [feat] 카카오 로그인 설정

--- boardpedia/boardpedia/Support/Delegate/AppDelegate.swift
+++ boardpedia/boardpedia/Support/Delegate/AppDelegate.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import AuthenticationServices
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let appleIDProvider = ASAuthorizationAppleIDProvider()
@@ -30,25 +32,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             default:
                 break
             }
+            
+            KakaoSDKCommon.initSDK(appKey: "1ea0e9d4d6122a3f57c96fb1255ce6b2")
         }
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        
+        return false
+    }
+    
+    
 }
 
